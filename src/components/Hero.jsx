1dba35c8db8832c2bc4686cece80b5bf87110dc8
cyber-h-u-b <cyberhub.startup@gmail.com>
2025-09-07 03:58:05 +0100

    Update project files by cIpH3r

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { Play, Shield, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import AwsVideoPlayer from "./AwsVideoPlayer";
-import bgImage from "@/assets/img/hero.png";
+import bgImage from "@/assets/img/hero.jpeg";
 import { EventRegistrationModal } from "./EventRegistrationModal"; // Import the consultation modal component
 
 export function Hero() {
@@ -122,8 +122,8 @@ export function Hero() {
           </Button>
         </div>
         <h1 className="mt-6 text-2xl font-bold text-white md:text-3xl lg:text-5xl">
-          Accelerate your career with expert-led training in Business Analysis,
-          Cybersecurity, Data Analytics, and Compliance.
+          At Titans Careers, growth and learning drive us.
+Explore opportunities to develop your skills and shape the future with us
         </h1>
         <p className="mt-4 max-w-2xl text-lg text-white">
           Get job-ready with Titans Careers. Learn global skills today and stand
@@ -131,7 +131,7 @@ export function Hero() {
         </p>
         <div className="mt-8 flex flex-col gap-4 sm:flex-row sm:items-center">
           <Link to="/courses" className="relative group overflow-hidden">
-            <Button className="w-full sm:w-auto px-8 py-6 bg-primary text-foreground hover:bg-transparent hover:text-white hover:border border-primary border rounded-full text-lg font-semibold shadow-xl hover:shadow-2xl transform transition-all duration-300">
+            <Button className="w-full sm:w-auto px-6 py-6 bg-primary text-foreground hover:bg-transparent hover:text-white hover:border border-primary border rounded-full text-lg font-semibold shadow-xl hover:shadow-2xl transform transition-all duration-300">
               Enrol Now
             </Button>
           </Link>

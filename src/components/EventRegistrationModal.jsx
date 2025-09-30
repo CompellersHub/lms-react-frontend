845bded1aac7706a7814d5812acd0ba875c3453b
cyber-h-u-b <cyberhub.startup@gmail.com>
2025-09-30 20:11:39 +0100

    Update project files by cIpH3r

--- src/components/EventRegistrationModal.jsx
+++ src/components/EventRegistrationModal.jsx
@@ -121,7 +121,7 @@ export function EventRegistrationModal({ isOpen, onClose, event, simple = false 
           </div>
           <div className="flex flex-col items-center mt-2 mb-2">
             <a
-              href="https://chat.whatsapp.com/BJnvGHfLUsxKsfAzKUrpxt"
+              href="https://chat.whatsapp.com/LbqHFDSOHT93o2cK1B4bBy"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-2 px-3 py-1 bg-green-500 hover:bg-green-600 text-white rounded-full font-semibold shadow transition-colors duration-200 text-sm"
